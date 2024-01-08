489734b49c7083ab9af68270c6529ee4b3dcbb2f
ufsilva2005 <ufsilva2005@gmail.com>
2024-01-08 17:13:12 -0300

    08-01-2024

---
+++ views/materialSaidas (cópia).php
@@ -0,0 +1,99 @@
+<?php
+    include "../template/menuPrincipal.php";
+    include "../scripts/mascara.php";
+    include_once "../funcao/funcao.php";
+    include_once "../dao/DAO-controleCir.php";
+
+    if ($_SESSION['entMaterial'] != "sim") {
+        echo "<script type='text/javascript'>alert('USUÁRIO NÃO AUTORIZADO');</script>";
+        echo "<script>location = '../template/menuPrincipal.php';</script>";
+    }
+
+    //Buscar código cir
+    $codigoDAO = new ControleCirDAO();
+    foreach ($codigoDAO->BuscarCodigo() as $resp) {
+        $codigo1 = $resp->codigo;
+    }
+
+    //echo strlen($codigo1);
+    $codigo2 = soNumero($codigo1);
+    $tamCodigo2 = strlen($codigo2);
+    $codigo2 = $codigo2 + 1;
+    $tamCodigo1 = strlen($codigo1);
+    $result = strstr($codigo1, "-", true);
+    $fimCodigo = str_pad($codigo2, $tamCodigo2, "0", STR_PAD_LEFT);
+    $codigo = $result . "-" . $fimCodigo;
+
+    //$idFuncionario = $_SESSION['idFuncionario'];  
+    //$nomeFuncionario = $_SESSION['nomeFuncionario'];  
+?>
+        <hr>
+        <nav class="navbar navbar-dark">
+            <div class="line col-md-12 p-5 position-absolute start-5 top-0 bottom-50 end-250">
+                <h3 class="text-success">Saída de Materiais:</h3>
+                <div class="panel-header">
+                    <form class="form-horizontal" action="" method="POST">
+                        <ul class="nav nav-pillsUfs mb-3" id="pillsUfs-tab" role="tablist">
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link active" id="pillsUfs-imp-tab" data-bs-toggle="pill" data-bs-target="#pillsUfs-imp" type="button" role="tab" aria-controls="pillsUfs-imp" aria-selected="true">Impressoras</button>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link" id="pillsUfs-comp-tab" data-bs-toggle="pill" data-bs-target="#pillsUfs-comp" type="button" role="tab" aria-controls="pillsUfs-comp" aria-selected="false">Computador</button>
+                            </li>
+                        </ul>
+
+                        <div class="tab-content" id="pillsUfs-tabContent">
+                            <div class="tab-pane fade show active" id="pillsUfs-imp" role="tabpanel" aria-labelledby="pillsUfs-imp-tab">
+                                <div class="row">
+                                    <div class="col px-md-1 col-md-5">
+                                        <label for="inputSuccess" class="control-label">Descrição:</label>
+                                        <input type="text" class="form-control" name="descImp" id="descImp" required>
+                                    </div>
+                                    <div class="col px-md-1 col-md-2">
+                                        <label for="inputSuccess" class="control-label">Código:</label>
+                                        <input type="text" class="form-control" name="codigoImp" id="codigoImp" value="<?= $codigo; $_SESSION['codigo'] = $codigo ?>" readonly />
+                                    </div>
+                                    <div class="col px-md-1 col-md-2">
+                                        <label for="inputSuccess" class="control-label">Nº de Série da Impressora:</label>
+                                        <input type="text" class="form-control" name="nunSerie" required />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="tab-pane fade" id="pillsUfs-comp" role="tabpanel" aria-labelledby="pillsUfs-comp-tab">
+                                <div class="row">
+                                    <div class="col px-md-1 col-md-5">
+                                        <label for="inputSuccess" class="control-label">Descrição:</label>
+                                        <input type="text" class="form-control" name="descComp" id="descImp" required>
+                                    </div>
+                                    <div class="col px-md-1 col-md-2">
+                                        <label for="inputSuccess" class="control-label">Código:</label>
+                                        <input type="text" class="form-control" name="codigoImp" id="codigoImp" value="<?= $codigo; $_SESSION['codigo'] = $codigo ?>" readonly />
+                                    </div>
+                                    <div class="col px-md-1 col-md-2">
+                                        <label for="inputSuccess" class="control-label">Nº do Computador:</label>
+                                        <input type="text" class="form-control" name="nunComp" required />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                        <div id="actions" class="row">
+                            <div class="col-md-2">
+                                <button type="submit" class="btn btn-success">Salvar</button>
+                            </div>
+
+                            <div class="col-md-2">
+                                <a href="../template/menuPrincipal.php"><button type="button" class="btn btn-outline-warning">Voltar</button></a>
+                            </div>
+
+                            <div class="col px-md-1 col-md-12" id="MostraPesq"> </div>
+                            <div class="col px-md-1 col-md-12" id="MostraPesq1"> </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </nav>        
+        <div class="bg-primary col-12 fixed-bottom p-5"></div>
+    </body>
+</html>
--- views/materialSaidas.php
+++ views/materialSaidas.php
@@ -1,99 +1,166 @@
-<?php
+  <?php
+    session_start();
     include "../template/menuPrincipal.php";
-    include "../scripts/mascara.php";
-    include_once "../funcao/funcao.php";
-    include_once "../dao/DAO-controleCir.php";
 
-    if ($_SESSION['entMaterial'] != "sim") {
+    if ($_SESSION['computador'] != "sim") {
         echo "<script type='text/javascript'>alert('USUÁRIO NÃO AUTORIZADO');</script>";
         echo "<script>location = '../template/menuPrincipal.php';</script>";
     }
 
-    //Buscar código cir
-    $codigoDAO = new ControleCirDAO();
-    foreach ($codigoDAO->BuscarCodigo() as $resp) {
-        $codigo1 = $resp->codigo;
-    }
-
-    //echo strlen($codigo1);
-    $codigo2 = soNumero($codigo1);
-    $tamCodigo2 = strlen($codigo2);
-    $codigo2 = $codigo2 + 1;
-    $tamCodigo1 = strlen($codigo1);
-    $result = strstr($codigo1, "-", true);
-    $fimCodigo = str_pad($codigo2, $tamCodigo2, "0", STR_PAD_LEFT);
-    $codigo = $result . "-" . $fimCodigo;
-
     //$idFuncionario = $_SESSION['idFuncionario'];  
     //$nomeFuncionario = $_SESSION['nomeFuncionario'];  
+
+    include "../scripts/validarNumImp.php";
+    include "../scripts/validarNumCir.php"; 
+    include "../scripts/validarNomeCir.php";
+    include "../scripts/validarMac.php"; 
+    include "../scripts/mascara.php";
+    include_once("../dao/conexao.php"); 
 ?>
+    <script src="../js/jquery.min.3-1.js"></script>
+    <script src="../js/bootstrap3-typeahead.min.js"></script>  
+    <script src="../js/jquery-latest.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#termo_busca').keyup(function() {
+                $.ajax({
+                    type: 'POST',
+                    url: '../scripts/buscaMaterial.php',
+                    data: {
+                        nome: $("#termo_busca").val()
+                    },
+                    success: function(data) {
+                        $('#listaMaterial').html(data);
+                    }
+                });
+            });
+
+        });
+    </script>
+    <script src="../js/codigoMat.js"></script>
+    
+
         <hr>
         <nav class="navbar navbar-dark">
             <div class="line col-md-12 p-5 position-absolute start-5 top-0 bottom-50 end-250">
-                <h3 class="text-success">Saída de Materiais:</h3>
-                <div class="panel-header">
-                    <form class="form-horizontal" action="" method="POST">
-                        <ul class="nav nav-pillsUfs mb-3" id="pillsUfs-tab" role="tablist">
-                            <li class="nav-item" role="presentation">
-                                <button class="nav-link active" id="pillsUfs-imp-tab" data-bs-toggle="pill" data-bs-target="#pillsUfs-imp" type="button" role="tab" aria-controls="pillsUfs-imp" aria-selected="true">Impressoras</button>
-                            </li>
-                            <li class="nav-item" role="presentation">
-                                <button class="nav-link" id="pillsUfs-comp-tab" data-bs-toggle="pill" data-bs-target="#pillsUfs-comp" type="button" role="tab" aria-controls="pillsUfs-comp" aria-selected="false">Computador</button>
-                            </li>
-                        </ul>
+                <i class="icon-tasks icon-blue"></i>
+                <h3 class="text-success">Cadastrar Saídas de Materiais</h3>
+                <div class="panel-content">
+                    <div class="col-md-12">
+                        <form name="cadastro" id="cadastro" method="post" action="../controllers/entradaMaterial.php">
+                            <fieldset>	                                
+                                <ul class="nav nav-pillsUfs mb-3" id="pillsUfs-tab" role="tablist">
+                                    <li class="nav-item" role="presentation">
+                                        <button class="nav-link active" id="pillsUfs-imp-tab" data-bs-toggle="pill" data-bs-target="#pillsUfs-imp" type="button" role="tab" aria-controls="pillsUfs-imp" aria-selected="true">Impressoras</button>
+                                    </li>
+                                    <li class="nav-item" role="presentation">
+                                        <button class="nav-link" id="pillsUfs-comp-tab" data-bs-toggle="pill" data-bs-target="#pillsUfs-comp" type="button" role="tab" aria-controls="pillsUfs-comp" aria-selected="false">Computador</button>
+                                    </li>
+                                </ul>
 
-                        <div class="tab-content" id="pillsUfs-tabContent">
-                            <div class="tab-pane fade show active" id="pillsUfs-imp" role="tabpanel" aria-labelledby="pillsUfs-imp-tab">
-                                <div class="row">
-                                    <div class="col px-md-1 col-md-5">
-                                        <label for="inputSuccess" class="control-label">Descrição:</label>
-                                        <input type="text" class="form-control" name="descImp" id="descImp" required>
+                                <div class="tab-content" id="pillsUfs-tabContent">
+                                    <div class="tab-pane fade show active" id="pillsUfs-imp" role="tabpanel" aria-labelledby="pillsUfs-imp-tab">
+                                        <div class="row">
+                                            <div class="col px-md-1 col-md-5">
+                                                <label for="inputSuccess" class="control-label">Descrição:</label>
+                                                <input type="text" class="form-control" name="descImp" id="descImp" required>
+                                            </div>
+                                            <div class="col px-md-1 col-md-2">
+                                                <label for="inputSuccess" class="control-label">Código:</label>
+                                                <input type="text" class="form-control" name="codigoImp" id="codigoImp" value="<?= $codigo; $_SESSION['codigo'] = $codigo ?>" readonly />
+                                            </div>
+                                            <div class="col px-md-1 col-md-2">
+                                                <label for="inputSuccess" class="control-label">Nº de Série da Impressora:</label>
+                                                <input type="text" class="form-control" name="nunSerie" required />
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div class="col px-md-1 col-md-2">
-                                        <label for="inputSuccess" class="control-label">Código:</label>
-                                        <input type="text" class="form-control" name="codigoImp" id="codigoImp" value="<?= $codigo; $_SESSION['codigo'] = $codigo ?>" readonly />
-                                    </div>
-                                    <div class="col px-md-1 col-md-2">
-                                        <label for="inputSuccess" class="control-label">Nº de Série da Impressora:</label>
-                                        <input type="text" class="form-control" name="nunSerie" required />
+
+                                    <div class="tab-pane fade" id="pillsUfs-comp" role="tabpanel" aria-labelledby="pillsUfs-comp-tab">
+                                        <div class="row">
+                                            <div class="col px-md-1  col-md-2">
+                                                <label for="inputSuccess" class="control-label">Pesquisar Material:</label>
+                                                <input type="text" class="form-control" id="termo_busca" name="termo_busca">               
+                                            </div>
+                                                                                
+                                            <div class="col px-md-1 col-md-4">
+                                                <label for="inputSuccess" class="control-label">Descrição do Material:</label>
+                                                <select class="form-control" name="listaMaterial" id="listaMaterial" onclick="if( $('#listaMaterial').html() == '' ){ alert('Material Não encontrado.\n Comunique ao Administrador do Sistema.');}" required></select>
+                                            </div>     
+
+                                            <div class="col px-md-1 col-md-3">
+                                                <label for="inputSuccess" class="control-label">Código:</label>
+                                                <input type="text" class="form-control"  name="codigo" id="codigo" value =" " readonly tabindex="-1">
+                                            </div>
+
+                                            <div class="col px-md-1 col-md-3">
+                                                <label for="inputSuccess" class="control-label">Quantidade:</label>
+                                                <input type="number" class="form-control" name="quantidadeMat" id="quantidadeMat" pattern="\d*"  min="1" required/>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col px-md-1  col-md-2">
+                                                <label for="inputSuccess" class="control-label">Nº do Computador:</label>
+                                                <input type="text" class="form-control" id="" name="" required>               
+                                            </div>
+                                                                                
+                                            <div class="col px-md-1 col-md-6">
+                                                <label for="inputSuccess" class="control-label">Nome do Técnico:</label>
+                                                <select class="form-control" name="" id="" required></select>
+                                            </div>  
+
+                                            <div class="col px-md-1 col-md-2">
+                                                <label for="inputSuccess" class="control-label">Data da Saída do Material:</label>
+                                                <select class="form-control" name="" id="" ></select>
+                                            </div>  
+                                        </div>  
                                     </div>
                                 </div>
-                            </div>
+                                <hr>
 
-                            <div class="tab-pane fade" id="pillsUfs-comp" role="tabpanel" aria-labelledby="pillsUfs-comp-tab">
-                                <div class="row">
-                                    <div class="col px-md-1 col-md-5">
-                                        <label for="inputSuccess" class="control-label">Descrição:</label>
-                                        <input type="text" class="form-control" name="descComp" id="descImp" required>
-                                    </div>
-                                    <div class="col px-md-1 col-md-2">
-                                        <label for="inputSuccess" class="control-label">Código:</label>
-                                        <input type="text" class="form-control" name="codigoImp" id="codigoImp" value="<?= $codigo; $_SESSION['codigo'] = $codigo ?>" readonly />
-                                    </div>
-                                    <div class="col px-md-1 col-md-2">
-                                        <label for="inputSuccess" class="control-label">Nº do Computador:</label>
-                                        <input type="text" class="form-control" name="nunComp" required />
-                                    </div>
+                                <div class="row">	
+                                    <label>
+                                        <h5 class="text-primary">Informações do responsavel</h5>
+                                    </label>	
                                 </div>
-                            </div>
-                        </div>
-                        <hr>
-                        <div id="actions" class="row">
-                            <div class="col-md-2">
-                                <button type="submit" class="btn btn-success">Salvar</button>
-                            </div>
 
-                            <div class="col-md-2">
-                                <a href="../template/menuPrincipal.php"><button type="button" class="btn btn-outline-warning">Voltar</button></a>
-                            </div>
+                                <div class="row">
+                                    <div class="col px-md-1  col-md-2">
+                                        <label for="inputSuccess" class="control-label">Data:</label>
+                                        <input type="text" class="form-control" value="<?php $date = date('d/m/Y'); echo $date; $_SESSION['data'] = $date ?>" disabled>
+                                    </div>
 
-                            <div class="col px-md-1 col-md-12" id="MostraPesq"> </div>
-                            <div class="col px-md-1 col-md-12" id="MostraPesq1"> </div>
-                        </div>
-                    </form>
+                                    <div class="col px-md-1 col-md-10">
+                                        <label for="inputSuccess" class="control-label">Nome:</label>
+                                        <input type="text" class="form-control" value="<?= $_SESSION['nomeFuncionario'] ?>" disabled>
+                                    </div>
+                                </div> 
+
+                                <hr>
+
+                                <div id="actions" class="row">
+                                    <div class="col-md-2">
+                                        <button type="submit" class="btn btn-success">Salvar</button>
+                                    </div>
+
+                                    <div class="col-md-2">
+                                        <a href="../template/menuPrincipal.php"><button type="button" class="btn btn-outline-warning">Voltar</button></a>
+                                    </div>
+
+                                    <div class="col px-md-1 col-md-12" id="MostraPesq"> </div>
+                                    <div class="col px-md-1 col-md-12" id="MostraPesq1"> </div>
+                                </div>                              
+                            </fieldset>
+                        </form>  
+                    </div>
+                    <p></p>
+                    <p></p>
+                    <footer>
+                        <p>&copy; <a href="">CADASTRO DE COMPUTADORES</a></p>
+                    </footer>
                 </div>
             </div>
-        </nav>        
-        <div class="bg-primary col-12 fixed-bottom p-5"></div>
+        </nav>
     </body>
 </html>
+        <
